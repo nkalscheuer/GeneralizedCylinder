bcaf0003cdd26283106b948a287e4f2e067bcda4
Nicholas Alan Kalscheuer <nkalsche@unix2.lt.ucsc.edu>
2018-04-29 23:39:40 -0700

    Editing features.html adding finishing touches

---
+++ src/HollowCylinder.js
@@ -0,0 +1,30 @@
+
+
+class ColoredCylinder extends OrthoCylinder {
+    constructor(start, end, radius, numOfSides, planeNormal = new Vector3([0, 0, 1]), color, lightSource){
+        super(start, end, radius, numOfSides, planeNormal = new Vector3([0, 0, 1]));
+        this.color = color;
+        this.lightSource = lightSource;
+    }
+    getColoredBuffer(lightSource = this.lightSource){
+        var coloredBuffer = [];
+        for(let i = 0; i < this.faces.length; i++){
+            let face = this.faces[i];
+            //console.log("Cylinder color: ");
+            //console.log(this.color);
+            let faceColor = face.getColor(this.color, lightSource).elements;
+            console.log("FaceColor");
+            console.log(faceColor);
+            //setting the same color to every vertex of a face
+            for(let j = 0; j < face.vertices.length; j++){
+                coloredBuffer.push(faceColor[0]);coloredBuffer.push(faceColor[1]);coloredBuffer.push(faceColor[2]);
+                //coloredBuffer.push(0);coloredBuffer.push(1);coloredBuffer.push(0);
+            }
+        }
+        return coloredBuffer;
+    }
+    changeColor(color){
+        this.color = color;
+    }
+    
+}
---
+++ src/LightSource.js
@@ -0,0 +1,7 @@
+class LightSource {
+    constructor(color, location, intensity){
+        this.color = color;
+        this.location = location;
+        this.intensity = intensity;
+    }
+}
